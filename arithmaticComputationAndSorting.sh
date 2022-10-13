b448523fc796183cc5510a21845437bffd0c8166
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 14:57:28 +0530

    Refector: Doing fourth arithmatic computation

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -5,7 +5,7 @@ read -p "Enter the first number: " a;
 read -p "Enter the second number: " b;
 read -p "Enter the third number: " c;
 
-operation_3=$(($c + $a/$b))
-echo "Value after computation is $operation_3"
+operation_4=$(($a%$b + $c))
+echo "Value after computation is $operation_4"
 
 
