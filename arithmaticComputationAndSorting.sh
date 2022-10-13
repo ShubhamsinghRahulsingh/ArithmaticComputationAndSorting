9794b40c81685a07a44f64fc9515a5ce997903db
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 13:08:43 +0530

    Added: Doing arithmatic computation

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -2,9 +2,10 @@
 
 
 read -p "Enter the first number: " a;
-echo $a
 read -p "Enter the second number: " b;
-echo $b
 read -p "Enter the third number: " c;
-echo $c
+
+operation=$(($a + $b*$c))
+echo "Value after computation is $operation"
+
 
