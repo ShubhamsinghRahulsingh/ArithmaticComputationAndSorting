da3bdcc9496f773e4cd55a7f1efdb53323e2d548
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 13:15:03 +0530

    Refactor: Doing arithmatic computation

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -5,7 +5,7 @@ read -p "Enter the first number: " a;
 read -p "Enter the second number: " b;
 read -p "Enter the third number: " c;
 
-operation=$(($a + $b*$c))
+operation=$(($a*$b + $c))
 echo "Value after computation is $operation"
 
 
