d04ab7f2250a8488785af771ad64450acf84eda0
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 15:28:35 +0530

    Added: Store the results in dictionary for every computation

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -1,11 +1,26 @@
-
-
+declare -A computation
 
 read -p "Enter the first number: " a;
 read -p "Enter the second number: " b;
 read -p "Enter the third number: " c;
-
+operation_1=$(($a + $b*$c))
+computation[operation1]=$operation_1
+operation_2=$(($a*$b + $c))
+computation[operation2]=$operation_2
+operation_3=$(($c + $a/$b))
+computation[operation3]=$operation_3
 operation_4=$(($a%$b + $c))
-echo "Value after computation is $operation_4"
+computation[operation4]=$operation_4
+
+echo "All operations are: ${computation[@]}"
+echo "First operation is: ${computation[operation1]}"
+echo "Second operation is: ${computation[operation2]}"
+echo "Third operation is: ${computation[operation3]}"
+echo "Fourth operation is: ${computation[operation4]}"
+
+
+
+
+
 
 
