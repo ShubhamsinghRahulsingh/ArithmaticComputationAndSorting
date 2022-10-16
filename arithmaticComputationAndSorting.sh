ce785bf5835c87396c54791b759ccf44f661353e
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-16 11:39:15 +0530

    Refector: Reading values from Dictionary into the array

--- arithmaticComputationAndSorting.sh
+++ arithmaticComputationAndSorting.sh
@@ -1,16 +1,21 @@
 declare -A computation
-
+declare -a array
+count=0
 read -p "Enter the first number: " a;
 read -p "Enter the second number: " b;
 read -p "Enter the third number: " c;
 operation_1=$(($a + $b*$c))
 computation[operation1]=$operation_1
+array[count]=${computation[operation1]}
 operation_2=$(($a*$b + $c))
 computation[operation2]=$operation_2
+array[$(($count+1))]=${computation[operation2]}
 operation_3=$(($c + $a/$b))
 computation[operation3]=$operation_3
+array[$(($count+2))]=${computation[operation3]}
 operation_4=$(($a%$b + $c))
 computation[operation4]=$operation_4
+array[$((count+3))]=${computation[operation4]}
 
 echo "All operations are: ${computation[@]}"
 echo "First operation is: ${computation[operation1]}"
@@ -18,6 +23,7 @@ echo "Second operation is: ${computation[operation2]}"
 echo "Third operation is: ${computation[operation3]}"
 echo "Fourth operation is: ${computation[operation4]}"
 
+echo "All operations are: ${array[@]}"
 
 
 
